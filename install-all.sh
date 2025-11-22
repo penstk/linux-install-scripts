b8ba194cc3c0eeebceeb45e5775ffcd30e863ac1
stk <git@stk.systems>
2025-11-22 10:57:37 +0100

    updated app paths/names

--- install-all.sh
+++ install-all.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Install all packages in order
-./apps/install-kitty.sh
-./apps/install-lazyvim.sh
-./apps/install-stow.sh
-./apps/spotify.sh
+# ./apps/install-kitty.sh
+./apps/nvim.sh
+./apps/stow.sh
+./apps/github-cli.sh
+# ./apps/spotify.sh
 # ./apps/install-dotfiles.sh
 # ./apps/install-tmux.sh
 # ./apps/install-zsh.sh
 
-./set-shell.sh
+# ./set-shell.sh
