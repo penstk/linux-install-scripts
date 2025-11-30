37119e1e5d6538bd52daee24cfbe839fe0f29e34
stk <git@stk.systems>
2025-11-30 03:36:31 +0100

    added quotes and -y to dnf install command

--- packages/discord.sh
+++ packages/discord.sh
@@ -23,7 +23,7 @@ install_package() {
     ;;
 
   fedora)
-    sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf install -y "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
     sudo dnf install -y discord
     ;;
 
