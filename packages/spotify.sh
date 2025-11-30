195501d7833283449565b80a62b2800a8ee9ebcb
stk <git@stk.systems>
2025-11-30 21:15:47 +0100

    added -y to fedora dnf install

--- packages/spotify.sh
+++ packages/spotify.sh
@@ -23,10 +23,10 @@ install_package() {
 
   fedora)
     # Enable rpmfusion repositories
-    sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-    sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
     # Install
-    sudo dnf install lpf-spotify-client
+    sudo dnf install -y lpf-spotify-client
     lpf update
     ;;
 
