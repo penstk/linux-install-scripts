e9a2fb6f5ae8054ab33feef934e168aa963574cb
stk <git@stk.systems>
2025-11-30 03:08:44 +0100

    added discord

---
+++ packages/discord.sh
@@ -0,0 +1,35 @@
+# Application name (used in logs / messages)
+APP_NAME="discord"
+
+# Command to check for in PATH.
+# Use a different value if the binary name differs from APP_NAME.
+CMD_NAME="$APP_NAME"
+
+# Load helper scripts
+. "$ROOT_DIR/helpers/is_installed.sh"
+. "$ROOT_DIR/helpers/install.sh"
+
+is_installed() {
+  is_installed_cmd "$CMD_NAME"
+}
+
+install_package() {
+  case "$DISTRO" in
+  arch | cachyos)
+    sudo pacman -S --needed --noconfirm discord
+    ;;
+  ubuntu)
+    sudo snap install dicord
+    ;;
+
+  fedora)
+    sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf install -y discord
+    ;;
+
+  *)
+    echo "$APP_NAME: Unsupported distro '$DISTRO'." >&2
+    return 1
+    ;;
+  esac
+}
