55d439b6ced470a3d0b68013bb172bf527db5a7f
stk <git@stk.systems>
2025-11-30 21:32:25 +0100

    added deps

--- packages/spotify.sh
+++ packages/spotify.sh
@@ -3,13 +3,39 @@ APP_NAME="spotify"
 
 # Command to check for in PATH.
 # Use a different value if the binary name differs from APP_NAME.
-CMD_NAME="spotify-launcher"
+CMD_NAME="$APP_NAME"
+CMD_NAME_ARCH="spotify-launcher"
+
+# Distro-specific dependencies:
+DEPENDENCIES=()
+case "$DISTRO" in
+ubuntu)
+  DEPENDENCIES+=(snapd)
+  ;;
+esac
 
 # Load helper scripts
 . "$ROOT_DIR/helpers/is_installed.sh"
 
 is_installed() {
-  is_installed_cmd "$CMD_NAME"
+  case "$DISTRO" in
+  arch | cachyos)
+    is_installed_cmd "$CMD_NAME_ARCH"
+    ;;
+
+  ubuntu)
+    is_installed_deps "${DEPENDENCIES[@]}" && is_installed_cmd "$CMD_NAME"
+    ;;
+
+  fedora)
+    is_installed_cmd "$CMD_NAME"
+    ;;
+
+  *)
+    echo "$APP_NAME: Unsupported distro '$DISTRO'." >&2
+    return 1
+    ;;
+  esac
 }
 
 install_package() {
@@ -17,6 +43,7 @@ install_package() {
   arch | cachyos)
     sudo pacman -S --needed --noconfirm spotify-launcher
     ;;
+
   ubuntu)
     sudo snap install spotify
     ;;
