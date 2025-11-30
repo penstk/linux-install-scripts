bd5838d98f91e8b3fdad03f482e51cc944b34e13
stk <git@stk.systems>
2025-11-30 03:33:59 +0100

    added vscode package

---
+++ packages/vscode.sh
@@ -0,0 +1,44 @@
+# Application name (used in logs / messages)
+APP_NAME="vscode"
+
+# Command to check for in PATH.
+CMD_NAME="code"
+
+# Distro-specific dependencies:
+DEPENDENCIES=()
+case "$DISTRO" in
+arch | cachyos)
+  DEPENDENCIES+=(paru)
+  ;;
+esac
+
+# Load helper scripts
+. "$ROOT_DIR/helpers/is_installed.sh"
+
+is_installed() {
+  is_installed_deps "${DEPENDENCIES[@]}" && is_installed_cmd "$CMD_NAME"
+}
+
+install_package() {
+  case "$DISTRO" in
+  arch | cachyos)
+    paru -S --needed --noconfirm visual-studio-code-bin
+    ;;
+
+  ubuntu)
+    sudo snap install --classic code
+    ;;
+
+  fedora)
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
+    dnf check-update
+    sudo dnf install -y code
+    ;;
+
+  *)
+    echo "$APP_NAME: Unsupported distro '$DISTRO'." >&2
+    return 1
+    ;;
+  esac
+}
