6f8fb786d0ebeb0f49b26eddf7f76f606757cc8b
stk <git@stk.systems>
2025-12-05 20:42:28 +0100

    disabled shellcheck for dependecies

--- packages/codex-cli.sh
+++ packages/codex-cli.sh
@@ -4,6 +4,7 @@ CMD_NAME="codex"
 
 # Packages that should be installed before installing this package.
 # Each entry must correspond to another package script in the packages directory (without .sh).
+# shellcheck disable=SC2034 # used by install.sh dependency resolver
 DEPENDENCIES=(
   nodejs
 )
