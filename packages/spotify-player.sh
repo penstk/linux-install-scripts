924bea460e16e07cbb3ea079012961efcd2656be
stk <git@stk.systems>
2025-12-05 23:01:11 +0100

    added ubuntu and fedora install via cargo

--- packages/spotify-player.sh
+++ packages/spotify-player.sh
@@ -35,10 +35,15 @@ install_package() {
     sudo pacman -S --needed --noconfirm spotify-player
     ;;
 
-  ubuntu | fedora)
+  ubuntu)
+    sudo apt-get install -y libssl-dev libasound2-dev libdbus-1-dev
     cargo install spotify_player --locked
     ;;
 
+  fedora)
+    sudo dnf install -y openssl-devel alsa-lib-devel dbus-devel
+    cargo install spotify_player --locked
+    ;;
   *)
     echo "$APP_NAME: Unsupported distro '$DISTRO'." >&2
     return 1
