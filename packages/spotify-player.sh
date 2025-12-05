7620c70b355b73e8e709826515342d377ac58bd5
stk <git@stk.systems>
2025-12-05 22:58:15 +0100

    added ubuntu and fedora install via cargo

--- packages/spotify-player.sh
+++ packages/spotify-player.sh
@@ -5,6 +5,14 @@ APP_NAME="spotify-player"
 # Use a different value if the binary name differs from APP_NAME.
 CMD_NAME="spotify_player"
 
+# Distro-specific dependencies:
+DEPENDENCIES=()
+case "$DISTRO" in
+ubuntu | fedora)
+  DEPENDENCIES+=(rustup)
+  ;;
+esac
+
 # Package names in each distro's package manager.
 # Set to "" if this package is not available on that distro.
 # Keep "$APP_NAME" when the package name matches APP_NAME.
@@ -22,4 +30,18 @@ is_installed() {
 
 install_package() {
   install_via_pkgmgr "$APP_NAME" "$ARCH_PKG" "$UBUNTU_PKG" "$FEDORA_PKG"
+  case "$DISTRO" in
+  arch | cachyos)
+    sudo pacman -S --needed --noconfirm spotify-player
+    ;;
+
+  ubuntu | fedora)
+    cargo install spotify_player --locked
+    ;;
+
+  *)
+    echo "$APP_NAME: Unsupported distro '$DISTRO'." >&2
+    return 1
+    ;;
+  esac
 }
