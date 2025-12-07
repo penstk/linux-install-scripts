a2406e115e8fe7d96f76f7cf2d41986fecbb9bf8
stk <git@stk.systems>
2025-12-07 16:06:35 +0100

    fixed fedora install

--- packages/7zip.sh
+++ packages/7zip.sh
@@ -5,21 +5,27 @@ APP_NAME="7-Zip"
 # Use a different value if the binary name differs from APP_NAME.
 CMD_NAME="7z"
 
-# Package names in each distro's package manager.
-# Set to "" if this package is not available on that distro.
-# Keep "$APP_NAME" when the package name matches APP_NAME.
-ARCH_PKG="7zip"
-UBUNTU_PKG="p7zip-full"
-FEDORA_PKG="p7zip p7zip-plugins"
-
 # Load helper scripts
 . "$ROOT_DIR/helpers/is_installed.sh"
-. "$ROOT_DIR/helpers/install.sh"
 
 is_installed() {
   is_installed_cmd "$CMD_NAME"
 }
 
 install_package() {
-  install_via_pkgmgr "$APP_NAME" "$ARCH_PKG" "$UBUNTU_PKG" "$FEDORA_PKG"
+  case "$DISTRO" in
+  arch | cachyos)
+    sudo pacman -S --needed --noconfirm "7zip"
+    ;;
+  ubuntu)
+    sudo apt-get install -y "p7zip-full"
+    ;;
+  fedora)
+    sudo dnf install -y "p7zip" "p7zip-plugins"
+    ;;
+  *)
+    echo "$app_name: Unsupported distro '$DISTRO'." >&2
+    return 1
+    ;;
+  esac
 }
