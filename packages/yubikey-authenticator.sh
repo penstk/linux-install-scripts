99f545e331ed5d45e264642f8ccd546d8f91f09d
stk <git@stk.systems>
2025-12-07 20:43:57 +0100

    split yubikey into authenticator an pam file

---
+++ packages/yubikey-authenticator.sh
@@ -0,0 +1,35 @@
+is_installed() {
+  case "$DISTRO" in
+  arch | cachyos)
+    is_installed_cmd "authenticator"
+    ;;
+  ubuntu)
+    is_installed_cmd "yubioath-desktop"
+    ;;
+  fedora)
+    flatpak info com.yubico.yubioath &>/dev/null
+    ;;
+  esac
+}
+
+install_package() {
+  case "$DISTRO" in
+  arch | cachyos)
+    # GUI tool to access and reset information, configure slot-based credentials, and read OATH codes from your YubiKey.
+    paru -S --needed --noconfirm yubico-authenticator
+    ;;
+
+  ubuntu)
+    # GUI tool to access and reset information, configure slot-based credentials, and read OATH codes from your YubiKey.
+    sudo apt-get install -y yubioath-desktop
+    ;;
+
+  fedora)
+    # GUI tool to access and reset information, configure slot-based credentials, and read OATH codes from your YubiKey.
+    sudo flatpak install -y flathub com.yubico.yubioath
+    ;;
+  esac
+
+  # Enable and start the pcscd daemon.
+  sudo systemctl enable --now pcscd
+}
