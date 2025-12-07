6167fba38310d3f6b737e6e3a1e7b96ffd50f907
stk <git@stk.systems>
2025-12-07 20:34:35 +0100

    added yubikey package

---
+++ packages/yubikey.sh
@@ -0,0 +1,42 @@
+# Application name (used in logs / messages)
+# Command to check for in PATH.
+# Use a different value if the binary name differs from APP_NAME.
+CMD_NAME="authenticator"
+
+# Load helper scripts
+. "$ROOT_DIR/helpers/is_installed.sh"
+
+is_installed() {
+  is_installed_cmd "$CMD_NAME"
+}
+
+install_package() {
+  case "$DISTRO" in
+  arch | cachyos)
+    # GUI tool to access and reset information, configure slot-based credentials, and read OATH codes from your YubiKey.
+    paru -S --needed --noconfirm yubico-authenticator
+
+    # PAM user authentication with U2F by Yubico. Supplement or replace password authentication with your YubiKey.
+    sudo pacman -S --needed --noconfirm -y pam-u2f
+    ;;
+
+  ubuntu)
+    # GUI tool to access and reset information, configure slot-based credentials, and read OATH codes from your YubiKey.
+    sudo apt-get install -y yubioath-desktop
+
+    # PAM user authentication with U2F by Yubico. Supplement or replace password authentication with your YubiKey.
+    sudo apt-get install -y libpam-u2f pamu2fcfg
+    ;;
+
+  fedora)
+    # GUI tool to access and reset information, configure slot-based credentials, and read OATH codes from your YubiKey.
+    sudo flatpak install -y flathub com.yubico.yubioath
+
+    # PAM user authentication with U2F by Yubico. Supplement or replace password authentication with your YubiKey.
+    sudo dnf install -y pam-u2f pamu2fcfg
+    ;;
+  esac
+
+  # Enable and start the pcscd daemon.
+  sudo systemctl enable --now pcscd
+}
