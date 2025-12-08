1f68add3ac30f8b0e853be217a321f7aee9d980c
stk <git@stk.systems>
2025-12-08 17:40:58 +0100

    added is_installed_asdf function

--- helpers/is_installed.sh
+++ helpers/is_installed.sh
@@ -1,5 +1,5 @@
 ################################################################################
-# Check if an application is installed
+# Check if an application is in PATH
 ################################################################################
 #
 #   $1 = app name
@@ -7,7 +7,42 @@
 ################################################################################
 is_installed_cmd() {
   local app_name=$1
-  command -v "$app_name" >/dev/null 2>&1
+  if ! command -v "$app_name" >/dev/null 2>&1; then
+    return 1
+  fi
+
+  return 0
+}
+
+################################################################################
+# Check if an application is installed via asdf-vm
+################################################################################
+#
+#   $1 = app name
+#
+################################################################################
+is_installed_asdf() {
+  local app_name=$1
+  local version=${2:-}
+
+  # Does the python plugin exist in asdf?
+  if ! asdf plugin list 2>/dev/null | grep -qx -- "$app_name"; then
+    return 1
+  fi
+
+  if [ -z "$version" ]; then
+    # No version given -> Does asdf have any version installed?
+    if ! asdf list "$app_name" >/dev/null 2>&1; then
+      return 1
+    fi
+  else
+    # Version given -> Check if the version appears anywhere in the output
+    if ! asdf list "$app_name" 2>/dev/null | grep -q -- "$version"; then
+      return 1
+    fi
+  fi
+
+  return 0
 }
 
 ################################################################################
