6571eaffbe1aaad466b1c0639005b8db0bba0516
stk <git@stk.systems>
2025-12-22 00:20:10 +0100

    adding mise shims to path

--- packages/mise.sh
+++ packages/mise.sh
@@ -14,6 +14,22 @@ is_installed() {
   is_installed_cmd "mise"
 }
 
+configure_shims() {
+  append_shell_env_block_if_missing "# mise shims integration" "$(
+    cat <<'EOF'
+# mise shims integration
+
+if [ -n "${BASH_VERSION-}" ]; then
+  eval "$(mise activate bash --shims)"
+elif [ -n "${ZSH_VERSION-}" ]; then
+  eval "$(mise activate zsh --shims)"
+fi
+EOF
+  )"
+
+  append_fish_env_line_if_missing 'mise activate fish --shims | source'
+}
+
 configure_shells() {
   append_shell_bash_line_if_missing 'eval "$(mise activate bash)"'
   append_shell_zsh_line_if_missing 'eval "$(mise activate zsh)"'
@@ -42,4 +58,5 @@ install_package() {
   esac
 
   configure_shells || return 1
+  configure_shims || return 1
 }
