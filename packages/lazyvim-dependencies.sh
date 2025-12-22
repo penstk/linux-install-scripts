2cb93f789fe6ae2a1b6783ce217b331f1e963c07
stk <git@stk.systems>
2025-12-22 23:49:30 +0100

    removed dependencies that are not required for lazyvim installation

--- packages/lazyvim-dependencies.sh
+++ packages/lazyvim-dependencies.sh
@@ -3,18 +3,18 @@
 
 DEPENDENCIES=(
   neovim
-  python3
   git                      # for partial clones support
   nerdfonts-jetbrains-mono # needed to display some icons
   lazygit
   curl # for nvim-treesitter
   gcc  # for nvim-treesitter
   # tree-sitter-cli # for nvim-treesitter
-  fzf           # for fzf-lua
-  ripgrep       # for fzf-lua
-  fd            # for fzf-lua
-  unzip         # for mason.nvim
-  neovim-pynvim # Should run last because it has brew dependencies which invalidate sudo keepalive
+  fzf     # for fzf-lua
+  ripgrep # for fzf-lua
+  fd      # for fzf-lua
+  unzip   # for mason.nvim
+  # python3
+  # neovim-pynvim
 )
 
 # Load helper scripts
